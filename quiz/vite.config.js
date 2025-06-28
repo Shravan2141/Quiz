bcf2bd10d1de73fa115bb6dca4fca43ab0ccdeb4
Shravan2141 <shravangupta878@gmail.com>
2025-06-28 16:25:24 +0530

    reverted

--- quiz/vite.config.js
+++ quiz/vite.config.js
@@ -1,13 +1,9 @@
-// import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.js
-import { defineConfig } from 'vite'
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Quiz/', // 👈 Repo name (must end with a /)
+  plugins: [react()],
 })
+vite.config.js
+
