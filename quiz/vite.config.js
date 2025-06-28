75a15892366dc0e6a547fa5c5dd262ee0f8b87e3
Shravan2141 <shravangupta878@gmail.com>
2025-06-28 16:18:45 +0530

    json and config

--- quiz/vite.config.js
+++ quiz/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
+import { defineConfig } from 'vite'
+
 export default defineConfig({
-  plugins: [react()],
+  base: '/Quiz/', // 👈 Repo name (must end with a /)
 })
